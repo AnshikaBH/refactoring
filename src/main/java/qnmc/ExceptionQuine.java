e075e1e5e73b28ab3090dac4be1655375e618353
Anshika <140836577+AnshikaBH@users.noreply.github.com>
2025-02-26 13:40:24 +0000

    Added @Serial annotation

--- src/main/java/qnmc/ExceptionQuine.java
+++ src/main/java/qnmc/ExceptionQuine.java
@@ -1,7 +1,10 @@
 package qnmc;
 
+import java.io.Serial;
+
 public class ExceptionQuine extends Exception {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     ExceptionQuine(String str) {
